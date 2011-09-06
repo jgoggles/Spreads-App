576c3bc92b5e7780e1ec7381ead4e66c5233f437
Dan Weaver <daniel.weaver@gmail.com>
2011-09-05 23:06:30 -0600

    final touches

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,16 @@
 require 'openid/store/filesystem'
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'MWkQcgKXGjfHDFW12LrE7g', 'fuv4ynPU1klPyQZzzSNOhUxN5PW1gx5kerum1jsvtyk'
-  provider :facebook, '210125529042474', 'ae3ef470ba50a122295d3eb72c0926ad'
+if Rails.env == "production"
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :twitter, 'oIdRcD6x1pwnC3aEm0TAHA', '8PWebacTPCMnDr5qpNgZRU2nhckOB6eejcoUIHS5K5w'
+    provider :facebook, '216823235041994', 'cfd885f760c9b19edfc6a345f0ca892c'
 
-  use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+    use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+  end 
+else
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :twitter, 'MWkQcgKXGjfHDFW12LrE7g', 'fuv4ynPU1klPyQZzzSNOhUxN5PW1gx5kerum1jsvtyk'
+    provider :facebook, '210125529042474', 'ae3ef470ba50a122295d3eb72c0926ad'
+
+    use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+  end
 end
-
