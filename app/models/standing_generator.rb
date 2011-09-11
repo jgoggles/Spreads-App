5899065206552f0e490dbe0b2f205ab604ae8c5d
Dan Weaver <daniel.weaver@gmail.com>
2011-09-11 01:40:13 -0600

    remove dev comments

--- app/models/standing_generator.rb
+++ app/models/standing_generator.rb
@@ -19,9 +19,9 @@ class StandingGenerator
       end
 
       Pool.all.each do |pool|
-        # if process_non_picks # only do these when the week is over, same as process non picks
+        if process_non_picks # only do these when the week is over, same as process non picks
           Delayed::Job.enqueue Badging.new(:pool, pool.id)
-        # end
+        end
         Standing.generate(pool.pick_sets)
         standings = Standing.for_season(pool.users, pool)
         REDIS.set("season_standings_#{pool.id}_#{Rails.env}", standings.to_json)
