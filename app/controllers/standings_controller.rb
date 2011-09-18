8e7a7b48ba3822d7b9f59062b80aafdf8fab7ec9
Dan Weaver <daniel.weaver@gmail.com>
2011-09-18 16:53:18 -0600

    standings fix

--- app/controllers/standings_controller.rb
+++ app/controllers/standings_controller.rb
@@ -13,7 +13,7 @@ class StandingsController < ApplicationController
   end
 
   def show
-    if params[:week_id].to_i >= Week.current.name.to_i
+    if params[:week_id].to_i >= Week.current.name.to_i and !@pool.all_picks_in
       raise ActionController::RoutingError.new('Not Found')
     else
       @week = Week.find_by_name(params[:week_id])
