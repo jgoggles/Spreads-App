415d60155de0fd1a844eeea58bbf9e171dd9600f
Dan Weaver <daniel.weaver@gmail.com>
2013-12-08 14:04:04 -0700

    try live

--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -5,7 +5,7 @@ class ScoreboardController < ApplicationController
   load_and_authorize_resource :pick_set, :through => :pool, :except => [:show, :edit, :update]
 
   def live
-    if @pool.all_picks_in
+    if @pool.all_picks_in || @week.pick_cutoff_passed?
       @pick_set = current_user.pick_set_for_this_week(@pool)
       @standings = JSON.parse(REDIS.get("season_standings_#{@pool.id}_#{Rails.env}")).sort_by { |i| -i['points'] }
     else
