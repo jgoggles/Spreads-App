d4edf7084b2a8809f8a309f00ca6547fb29f348e
Dan Weaver <daniel.weaver@gmail.com>
2019-09-07 12:33:49 -0600

    api changes for handling week 1

--- app/controllers/api/v1/access_controller.rb
+++ app/controllers/api/v1/access_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::AccessController < Api::BaseController
   def picks_available
     @pool = Pool.find_by(hashed_id: params[:pool])
-    show = (Week.current.pick_cutoff_passed? || @pool.all_picks_in) && !@pool.preseason?
+    if Rails.env == "production"
+      show = (Week.current.pick_cutoff_passed? || @pool.all_picks_in) && !@pool.preseason?
+    else
+      show = true
+    end
     render json: {show_picks: show}
   end
 end
--- app/controllers/api/v1/picks_controller.rb
+++ app/controllers/api/v1/picks_controller.rb
@@ -2,11 +2,19 @@ class Api::V1::PicksController < Api::BaseController
   def index
     @pool = Pool.find_by(hashed_id: params[:pool])
     @user = User.find_by(hashed_id: params[:user])
-    @standings = JSON.parse(REDIS.get("season_standings_#{@pool.id}_#{Rails.env}")).sort_by { |i| -i['points'] }
+    if @week.is_week_one?
+      @standings = []
+    else
+      @standings = JSON.parse(REDIS.get("season_standings_#{@pool.id}_#{Rails.env}")).sort_by { |i| -i['points'] }
+    end
     @pick_sets = @pool.pick_sets.where(week: Week.current)
     @pick_sets = @pick_sets.map do |pick_set| 
       standing = @standings.select { |s| s["player"]["id"] == pick_set.user_id } 
-      pick_set.points = standing.first["points"]
+      if @week.is_week_one?
+        pick_set.points = 0
+      else
+        pick_set.points = standing.first["points"]
+      end
       pick_set
     end
   end
