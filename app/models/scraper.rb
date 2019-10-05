92b7843b0925ada90e21dab307e7dca91aed2c77
Dan Weaver <daniel.weaver@gmail.com>
2019-10-05 14:48:23 -0600

    update score scraper

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -48,17 +48,19 @@ class Scraper
       data = JSON.load(open(url))
 
       data.each do |k, v|
+        v["home"]["abbr"].gsub!(/LA$/, "LAR")
         parsed_home_team = v["home"]["abbr"]
         home_score = v["home"]["score"]["T"]
         parsed_away_team = v["away"]["abbr"]
+        v["away"]["abbr"].gsub!(/LA$/, "LAR")
         away_score = v["away"]["score"]["T"]
         puts "#{parsed_away_team} #{away_score} - #{parsed_home_team} #{home_score}"
 
-        home_team = Team.find_by(abbr: parsed_home_team).games.where("week_id = ?", week.id).first.home
+        home_team = Team.find_by(abbr: parsed_home_team).games.where(week_id: week.id).first.home
         home_team.score = home_score
         home_team.save
 
-        away_team = Team.find_by(abbr: parsed_away_team).games.where("week_id = ?", week.id).first.away
+        away_team = Team.find_by(abbr: parsed_away_team).games.where(week_id: week.id).first.away
         away_team.score = away_score
         away_team.save
       end
